170a3fe3511719a86d4c98bd06d60a57280c5bd4
Akil <145994948+Akil7204@users.noreply.github.com>
2025-03-04 16:09:41 +0530

    Time: 20 ms (56.72%) | Memory: 60.5 MB (26.89%) - LeetSync

---
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function (list1, list2) {
+    const common = list1.filter(item => list2.includes(item));
+    const index1 = common.map(item => list1.indexOf(item) + list2.indexOf(item));
+    const min = Math.min(...index1);
+    return common.filter((item, index) => index1[index] === min);
+};
