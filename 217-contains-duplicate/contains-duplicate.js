7c6ac0dab190daffb8cf27aaf2c658ade1494750
Akil <145994948+Akil7204@users.noreply.github.com>
2025-09-12 08:47:50 +0530

    Time: 35 ms (25.45%) | Memory: 75.6 MB (32.14%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+
+    let obj = {};
+
+    for(let num of nums){
+        if(obj[num]){
+            return true
+        } else {
+            obj[num] = 1;
+        }
+    }
+    return false
+};
