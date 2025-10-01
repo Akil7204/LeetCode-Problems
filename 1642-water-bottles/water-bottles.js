82b8207fa61f45539f9150e9d856aae422b0a11d
Akil <145994948+Akil7204@users.noreply.github.com>
2025-10-01 13:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (52.29%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var numWaterBottles = function(numBottles, numExchange) {
+    return numBottles + Math.floor((numBottles - 1) / (numExchange - 1));
+    
+};
